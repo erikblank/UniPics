6f2fcc8ed4263a16ab31c9a10dfda1d76286f328
erik <erikblank97@web.de>
2019-08-29 23:55:02 +0200

    added persistent bottom-sheet

--- app/src/main/java/com/example/unipics/ImageActivity/ImageActivity.java
+++ app/src/main/java/com/example/unipics/ImageActivity/ImageActivity.java
@@ -1,12 +1,16 @@
 package com.example.unipics.ImageActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.example.unipics.NoteActivity.NoteActivity;
 import com.example.unipics.R;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.squareup.picasso.Picasso;
 
 import static com.example.unipics.Constants.KEY_IMAGE;
@@ -19,7 +23,11 @@ public class ImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image);
         String uri = getIntent().getStringExtra(KEY_IMAGE);
 
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+
+        BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         PhotoView photoView = findViewById(R.id.photo_view);
         Picasso.get().load(Uri.parse(uri)).into(photoView);
+
     }
 }
